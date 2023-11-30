eebcc003f25cd4041bf5ee12e83b986d2c5ae535
pryority <matthewapryor@gmail.com>
2023-11-30 17:40:06 -0500

    💄 Update theme color and button style

--- src/app/_components/ui/button.tsx
+++ src/app/_components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-[10px] text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         // primary:
         //   "bg-cyan text-cyan-foreground border border-cyan-600 hover:text-cyan-200 hover:border-cyan-800 hover:bg-cyan-700/90",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "text-secondary-foreground bg-secondary hover:bg-secondary/90",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
